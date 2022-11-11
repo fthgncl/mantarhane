4738a2ef093017269e6a2b7ebcfcb61cd27d384e
fthgncl <gencal.fatih61@gmail.com>
2022-11-11 19:02:06 +0300

    PHP sql kontrol oluşturuldu

---
+++ php/sql_komut.php
@@ -0,0 +1,26 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] != "POST"){
+    break;
+}
+
+$data = json_decode(file_get_contents('php://input'), true);
+$dataBase = veriTabaninaBaglan($data['username'], $data['password'], $data['databaseName']);
+veriTabaniKomutGonder($dataBase, $data['command']);
+
+function veriTabaninaBaglan($username, $password, $databaseName)
+{
+    $servername = "localhost";
+
+    $conn = new mysqli($servername, $username, $password, $databaseName);
+    if ($conn->connect_error) {
+        die("Veri tabanı bağlantısı başarısız oldu : " . $conn->connect_error);
+    }
+    $conn->set_charset("utf8");
+    return $conn;
+}
+
+function veriTabaniKomutGonder($dataBase, $command)
+{
+    return $dataBase->query($command);
+}
+?>
