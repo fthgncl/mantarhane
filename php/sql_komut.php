1fbe87a16ad601a62b12642aca5f46d623f4af92
fthgncl <gencal.fatih61@gmail.com>
2022-11-11 23:20:41 +0300

    mysql çıkışı eklendi

--- php/sql_komut.php
+++ php/sql_komut.php
@@ -6,6 +6,7 @@ if ($_SERVER["REQUEST_METHOD"] != "POST"){
 $data = json_decode(file_get_contents('php://input'), true);
 $dataBase = veriTabaninaBaglan($data['username'], $data['password'], $data['databaseName']);
 veriTabaniKomutGonder($dataBase, $data['command']);
+mysqli_close($dataBase);
 
 function veriTabaninaBaglan($username, $password, $databaseName)
 {
@@ -23,4 +24,5 @@ function veriTabaniKomutGonder($dataBase, $command)
 {
     return $dataBase->query($command);
 }
+
 ?>
