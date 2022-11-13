e6a2013f31599a4ccb5f2c3fc7b71ab6a2539b71
fthgncl <gencal.fatih61@gmail.com>
2022-11-14 00:21:34 +0300

    ışık kontrolündeki hata giderildi

--- arduino/isik_control.ino
+++ arduino/isik_control.ino
@@ -2,9 +2,9 @@ bool isik_mod;  // 0 : bekleme modunda , 1 : çalışma modunda
 int isik_sureler[2]; // 0 : bekleme suresi , 1 : calışma süresi
 void isik_loop() {
   isik_sureler[isik_mod]++;
-  if ( isik_sureler[isik_mod] > calismaPlani[isik_mod ? ISIK_BEKLEME_SURESI : ISIK_CALISMA_SURESI]) {
+  if ( isik_sureler[isik_mod] > calismaPlani[isik_mod ? ISIK_CALISMA_SURESI : ISIK_BEKLEME_SURESI]) {
     isik_sureler[isik_mod] = 0;
     isik_mod = !isik_mod;
-    digitalWrite(ISIK,isik_mod?HIGH:LOW);
+    digitalWrite(PIN_ISIK,isik_mod?HIGH:LOW);
   }
 }
