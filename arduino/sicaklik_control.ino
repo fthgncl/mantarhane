e6b79f34c5a374ad11b31681bb13bc44b4169a11
fthgncl <gencal.fatih61@gmail.com>
2022-11-14 13:51:29 +0300

    oluşabilecek ısı sensörü hatası giderildi

--- arduino/sicaklik_control.ino
+++ arduino/sicaklik_control.ino
@@ -1,4 +1,4 @@
-float sicaklikDegeleri[4];
+float sicaklikDegerleri[4];
 uint8_t sensorAdresleri[][8] = {
   { 0x28, 0x3F, 0xD4, 0x56, 0xB5, 0x01, 0x3C, 0x17 },
   { 0x28, 0x79, 0x54, 0x94, 0x97, 0x03, 0x03, 0x90 },
@@ -10,24 +10,32 @@ void sicaklik_control_setup() {
 }
 
 void sicaklik_control_loop() {
-  sicaklikOlcumuYap();
-  int OrtalamaSicaklik = ortalamaSicakligiHesapla();
-  Serial.println(OrtalamaSicaklik);
-  digitalWrite(PIN_KLIMA_SOGUTMA, OrtalamaSicaklik > calismaPlani[SICAKLIK_UST_DEGER] ?HIGH:LOW);
-  digitalWrite(PIN_KLIMA_ISITMA,  OrtalamaSicaklik < calismaPlani[SICAKLIK_ALT_DEGER] ?HIGH:LOW);
-  
+  if ( sicaklikOlcumuYap() ) {
+    int OrtalamaSicaklik = ortalamaSicakligiHesapla();
+    digitalWrite(PIN_KLIMA_SOGUTMA, OrtalamaSicaklik > calismaPlani[SICAKLIK_UST_DEGER] ? HIGH : LOW);
+    digitalWrite(PIN_KLIMA_ISITMA,  OrtalamaSicaklik < calismaPlani[SICAKLIK_ALT_DEGER] ? HIGH : LOW);
+  }
 }
 int ortalamaSicakligiHesapla() {
   int ortalamaSicaklik = 0;
-  for ( byte i = 0 ; i < ARRAY_SIZE(sicaklikDegeleri) ; i++) {
-    ortalamaSicaklik += sicaklikDegeleri[i];
+  for ( byte i = 0 ; i < ARRAY_SIZE(sicaklikDegerleri) ; i++) {
+    ortalamaSicaklik += sicaklikDegerleri[i];
   }
-  ortalamaSicaklik /= ARRAY_SIZE(sicaklikDegeleri);
+  ortalamaSicaklik /= ARRAY_SIZE(sicaklikDegerleri);
   return ortalamaSicaklik;
 }
-void sicaklikOlcumuYap() {
+bool sicaklikOlcumuYap() {
+  bool hatasizOlcum = true;
   sensors.requestTemperatures();
-  for ( byte i = 0 ; i < ARRAY_SIZE(sicaklikDegeleri) ; i++) {
-    sicaklikDegeleri[i] = sensors.getTempC(sensorAdresleri[i]);
+  
+  for ( byte i = 0 ; i < ARRAY_SIZE(sicaklikDegerleri) ; i++) {
+    sicaklikDegerleri[i] = sensors.getTempC(sensorAdresleri[i]);
+    Serial.println(sicaklikDegerleri[i]);
+
+    if ( sicaklikDegerleri[i] < -20 || sicaklikDegerleri[i] > 70 ) {
+      hatasizOlcum = false;
+    }
   }
+  
+  return hatasizOlcum;
 }
